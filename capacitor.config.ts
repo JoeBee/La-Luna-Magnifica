120702479f5f70a1fccd24b4ad362160066ffc7a
Your Name <you@example.com>
2025-04-15 13:51:16 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'La-Luna-Magnifica',
+  webDir: 'www'
+};
+
+export default config;
